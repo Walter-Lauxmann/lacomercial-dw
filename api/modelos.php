ff867f0551d30e003dc7d2d9cdb04e1871fa0884
Walter Lauxmann <walterlauxmann@institutocedec.com>
2023-08-01 20:30:42 -0300

    Insertar datos - Parte 1 - Clase de 01-08

--- api/modelos.php
+++ api/modelos.php
@@ -78,7 +78,9 @@ class ModeloABM extends Modelo {
     }
 
 
-    /** Método de Selección de datos */
+    /** 
+     * Método de Selección de datos 
+     */
     public function seleccionar() {
         // SELECT * FROM articulos WHERE criterio ORDER BY campo LIMIT 10
         $sql = "SELECT $this->campos FROM $this->tabla"; // SELECCIONAR $campos DESDE $tabla
@@ -108,7 +110,31 @@ class ModeloABM extends Modelo {
 
         // Retornamos los datos JSON
         return $datos_json;
+    }
 
+    /**
+     * Método de Inserción datos
+     * @param valores los valores a insertar
+     */
+    public function insertar($valores) {
+        // INSERT INTO articulos(codigo, nombre, descripcion, precio, imagen)
+        // VALUES ('201', 'Samsung Galaxy S20', 'Procesador: xxx, Almacenamiento: xxx', '890000', 'samsung-galaxy-s20.jpg')
+        $campos = ''; // Variable para almacenar los campos
+        $datos = '';  // Variable para almacenar los datos
+
+        // Recorrer el objeto $valores
+        foreach($valores as $key=>$value) {
+            $value = "'".$value."'"; // Agregamos apóstrofe (') antes y después de cada value
+            $campos .= $key.","; // Agregamos en $campos, la $key más una coma
+            $datos .= $value.","; // Agregamos en $datos, los $value más una coma
+        }
+        $campos = substr($campos,0,strlen($campos)-1); // Quitamos el último caracter (,) a $campos
+        $datos = substr($datos,0,strlen($datos)-1); // Quitamos el último caracter (,) a $datos
+
+        // Instrucción SQL
+        $sql = "INSERT INTO $this->tabla($campos) VALUES($datos)";
+        echo $sql; // Mostramos la instrucción SQL resultante
+        $this->_db->query($sql); // Ejecutamos la instrucción SQL
     }
     
 
