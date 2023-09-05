75330d444ebeab28c69c3d9587d11ca318fd7766
Walter Lauxmann <walterlauxmann@institutocedec.com>
2023-09-05 20:33:39 -0300

    Subir imágenes - clase del 05-09

--- api/datos.php
+++ api/datos.php
@@ -12,21 +12,47 @@ if (isset($_GET['tabla'])) { // Si está seteado el atributo tabla
     }
 
     if(isset($_GET['accion'])) { // Si está seteado el atributo accion
+        if($_GET['accion'] == 'insertar' || $_GET['accion'] == 'actualizar') {
+            $valores = $_POST;                  // Tomamos los valores del POST
+        }
+
+        // Subida de imágenes
+        if(                                     // si
+            isset($_FILES) &&                      // está seteado $_FILES Y
+            isset($_FILES['imagen']) &&            // está seteado $_FILES['imagen'] Y
+            !empty($_FILES['imagen']['name'] &&    // NO está vacío $_FILES['imagen']['name'] Y
+            !empty($_FILES['imagen']['tmp_name'])) // NO está vacío $_FILES['imagen']['tmp_name']
+            ) {
+            if(is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+                $tmp_nombre = $_FILES['imagen']['tmp_name'];
+                $nombre = $_FILES['imagen']['name'];
+                $destino = '../imagenes/productos/' . $nombre;
+                if(move_uploaded_file($tmp_nombre, $destino)) { // Si podemos mover el archivo temporal al destino
+                    $mensaje .= 'Archivo subido correctamente a ' . $destino;
+                    $valores['imagen'] = $nombre;
+                } else {
+                    $mensaje .= 'No se ha podido subir el archivo';
+                    unlink(ini_get('upload_tmp_dir').$_FILES['imagen']['tmp_name']);
+                }
+            } else {
+                $mensaje .= 'Error: El archivo no fue procesado correctamente';
+            }
+        }
+
+
         switch($_GET['accion']) {               // Según la accion
             case 'seleccionar':                     // En caso que sea 'seleccionar'
                $datos = $tabla->seleccionar();      // Ejecutamos el método seleccionar()
                 echo $datos;                        // Devolvemos los datos
                 break;
                 
-            case 'insertar':                        // En caso que sea 'insertar'
-                $valores = $_POST;                  // Tomamos los valores del POST
+            case 'insertar':                        // En caso que sea 'insertar'                
                 $tabla->insertar($valores);         // Ejecutamos el método insertar()
                 $mensaje .= 'Datos guardados';      // Creamos un mensaje
                 echo json_encode($mensaje);         // Mostramos el mensaje
                 break;
 
-            case 'actualizar':                      // En caso que sea 'actualizar'
-                $valores = $_POST;                  // Tomamos los valores del POST
+            case 'actualizar':                      // En caso que sea 'actualizar'                
                 $tabla->actualizar($valores);       // Ejecutamos el método actualizar()
                 $mensaje .= 'Datos actualizados';   // Creamos un mensaje
                 echo json_encode($mensaje);         // Mostramos el mensaje
